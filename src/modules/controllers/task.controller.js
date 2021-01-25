3ddc7f16aa66ade4b4bbbea80067b11236bfedd4
Darya Usatova <ysatodashava@gmail.com>
2021-01-25 16:20:29 +0300

    Updated readme. Created check for fields.

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -5,25 +5,43 @@ module.exports.getAllTasks = async (req, res, next) => {
 };
 
 module.exports.createNewTask = (req, res, next) => {
-  req.body.id = tasks.length + 1;
-  tasks.push(req.body);
-  res.send({data: tasks});
+  const body = req.body;
+  if (body.hasOwnProperty('text') && body.hasOwnProperty('isCheck')) {
+    body.id = tasks.length + 1;
+    tasks.push(body);
+    res.send({data: tasks});
+  } else {
+    res.status(422).send('Error! Params not correct');
+  }
 };
 
 module.exports.changeTaskInfo = (req, res, next) => {
-  tasks.forEach((item, i) => {
-    if(item.id === req.body.id) {
-      tasks[i] = req.body;
-    }
-  });
-  res.send({data: tasks});
+  const body = req.body;
+  if (body.hasOwnProperty('id') && (body.hasOwnProperty('text') || body.hasOwnProperty('isCheck'))) {
+    tasks.forEach((item, i) => {
+      if(item.id === body.id) {
+        for(let key in body) {
+          tasks[i][key] = body[key];
+        }
+      }
+    });
+    res.send({data: tasks});
+  } else {
+    res.status(422).send('Error! Params not correct');
+  }
 };
 
 module.exports.deleteTask = (req, res, next) => {
-  tasks.forEach((item, i) => {
-    if(item.id === +req.query.id ) {
-      tasks.splice(i, 1);
-    }
-  });
-  res.send({data: tasks});
+  if (!req.query.id) return res.status(422).send('Error! Params not correct');
+  const task = tasks.filter(item => item.id === +req.query.id);
+  if (task.length) {
+    tasks.forEach((item, i) => {
+      if(item.id === +req.query.id) {
+        tasks.splice(i, 1);
+      }
+    });
+    res.send({data: tasks});
+  } else {
+    res.status(404).send('Task not found');
+  }
 };
